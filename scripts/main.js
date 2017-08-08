1013e140d787722109b2b8b5bb26e0b793d0d00a
Jon Burr <yojonathanwburr@gmail.com>
2017-08-08 11:32:48 -0700

    Add translation sections, anchor scroll, share meta

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,6 @@ require('jquery');
 require('venobox');
 var test = require('./hello');
 test.hello();
-var scroll = require('./scroll');
-scroll.scroll();
 
 $(function() {
   console.log("HELLO jQuery and Modernizr loaded");
@@ -31,10 +29,51 @@ $(document).ready(function() {
      }
   });
 
+  // Select all links with hashes
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        //event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
-  //check for hash on page load
+  function resolveHash() {
+    //check for hash on page load
+    $('body').addClass('consolidate-header');
+  }
+  
   if (window.location.hash) {
     window.console.log('hash exists', window.location.hash);
+    resolveHash();
   }
 
   //Instantiate venobox
