5ebb0c69e6eb04db80fd47de8598429a3dce9e4c
Tom Beck <tom@tastylounge.com>
2017-08-17 11:20:37 -0700

    updated form submissions

--- scripts/animations.js
+++ scripts/animations.js
@@ -7,20 +7,20 @@ $('.illustration').each(function() {
   new Waypoint.Inview({
     element: this,
     enter: function(direction) {
-      window.console.log('Enter triggered with direction ' + direction);
+      // window.console.log('Enter triggered with direction ' + direction);
       $(this.element).find('img').attr('class', 'animated fadeInUp');
     },
     entered: function(direction) {
-      window.console.log('Entered triggered with direction ' + direction);
+      // window.console.log('Entered triggered with direction ' + direction);
       $(this.element).find('svg').attr('class', 'animated pulse');
     },
     exit: function(direction) {
-      window.console.log('Exit triggered with direction ' + direction);
+      // window.console.log('Exit triggered with direction ' + direction);
       $(this.element).find('svg').attr('class', 'animated');
       $(this.element).find('img').attr('class', 'animated');
     },
     exited: function(direction) {
-      window.console.log('Exited triggered with direction ' + direction);
+      // window.console.log('Exited triggered with direction ' + direction);
     }
   });
 });
