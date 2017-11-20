b820c794563162ab756268d39803af38dff6c96e
Ben Dean-Kawamura <ben@pculture.org>
2017-11-20 10:43:55 -0500

    Fixes for filter lists
    - Give the correct width for the page
    - Add more font sizes for Raleway, so we get the correct font
    - Include application/_variables.scss so we get the correct color

--- scripts/head/head.js
+++ scripts/head/head.js
@@ -3,8 +3,8 @@ var WebFont = require('webfontloader');
 WebFont.load({
   google: {
     families: [
-        'Open Sans',
-        'Raleway',
+        'Open Sans:300,300i,400,400i,600,600i,700,700i,800,800i',
+        'Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i',
     ]
   },
   loading: function() {
