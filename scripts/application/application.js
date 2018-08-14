29d74f69bc8b31ddefdb78c4b7f1834f32bbf158
Ben Dean-Kawamura <ben@pculture.org>
2018-08-14 14:31:54 -0400

    Added CSS/JS for dependent checkboxes
    Also updated the regular radiobutton/checkbox styles.  Made them
    slightly bigger.

--- scripts/application/application.js
+++ scripts/application/application.js
@@ -29,3 +29,4 @@ require('../shared/tabs');
 require('../shared/eventTracking');
 require('../shared/teamRoleSelect');
 require('../shared/videoCreatePage');
+require('../shared/dependentCheckboxes');
